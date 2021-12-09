4a3d13d3118dc0bbe3c1577037e51cb9c82e57a7
pskry <peter@skrypalle.dk>
2021-12-09 21:31:34 +0100

    Initial commit

---
+++ checkout.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+if ! eval which git >/dev/null 2>&1; then
+    1>&2 echo "'git' not found. Please check your path or install git."
+    exit 1
+fi
+
+GIT="$(which git)"
+WORK_DIR="$HOME"
+TARGET_DIR="$WORK_DIR/.dotfiles"
+BACKUP_DIR="$WORK_DIR/.dotfiles-backup"
+UPSTREAM="git@github.com:pskry/dotfiles.git"
+UPSTREAM="https://github.com/pskry/dotfiles.git"
+
+dot() {
+    $GIT --git-dir=$TARGET_DIR --work-tree=$WORK_DIR $@
+}
+
+$GIT clone --bare $UPSTREAM $TARGET_DIR || exit 1
+
+dot checkout 2>/dev/null
+success=$?
+if [ $success -ne 0 ]; then
+    echo "backing up pre-existing dotfiles..."
+    conflicts=$(dot checkout 2>&1 | grep -E "\s+\." | awk '{print $1}')
+
+    # check all backup moves
+    for item in ${conflicts[@]}; do
+	src="$WORK_DIR/$item"
+	dst="$BACKUP_DIR/$item"
+        if [ -f $dst ]; then
+		1>&2 echo "cannot backup '$src' - destination file ($dst) already exists! please move the destination file or choose another backup location."
+	    exit 1
+	fi
+    done
+
+    # commit backup move
+    for item in ${conflicts[@]}; do
+	src="$WORK_DIR/$item"
+	dst="$BACKUP_DIR/$item"
+        echo "  backup $item"
+	mkdir -p $(dirname $dst)
+        mv $src $dst
+    done
+fi
+
+# checkout again after failure and subsequent backup
+[ $success -eq 0 ] || dot checkout
+if [ $? -ne 0 ]; then
+	1>&2 echo "unexpected error. abort"
+fi
+
+# hide all untracked files
+dot config status.showUntrackedFiles no
+
+echo "dotfiles successfully checked out"
+echo "convenience alias for managing dotfiles:"
+echo "  alias dot='git --git-dir=$TARGET_DIR --work-tree=$WORK_DIR'"
