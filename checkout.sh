d5bd5e9c3f0d82fd8ac8a16accd771ef9c659599
pskry <peter@skrypalle.dk>
2021-12-12 16:20:03 +0100

    add branch cmdline arg - no usage/help yet sorry

--- checkout.sh
+++ checkout.sh
@@ -5,18 +5,22 @@ if ! eval which git >/dev/null 2>&1; then
     exit 1
 fi
 
+BRANCH="master"
+if [ $# -gt 0 ]; then
+    BRANCH="$1"
+fi
+
 GIT="$(which git)"
-WORK_DIR="$HOME"
+WORK_DIR="$HOME/yolo"
 TARGET_DIR="$WORK_DIR/.dotfiles"
 BACKUP_DIR="$WORK_DIR/.dotfiles-backup"
 UPSTREAM="git@github.com:pskry/dotfiles.git"
-UPSTREAM="https://github.com/pskry/dotfiles.git"
 
 dot() {
     $GIT --git-dir=$TARGET_DIR --work-tree=$WORK_DIR $@
 }
 
-$GIT clone --bare $UPSTREAM $TARGET_DIR || exit 1
+$GIT clone -b $BRANCH --bare $UPSTREAM $TARGET_DIR || exit 1
 
 dot checkout 2>/dev/null
 success=$?
@@ -52,6 +56,10 @@ fi
 
 # hide all untracked files
 dot config status.showUntrackedFiles no
+
+# resolve submodules
+dot submodule init
+dot submodule update
 
 echo "dotfiles successfully checked out"
 echo "convenience alias for managing dotfiles:"
